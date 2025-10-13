9be45cd6c39c6e804cda085377e4426996690f52
rdlassoe <robinsonlasso2021@itp.edu.co>
2025-10-13 18:53:23 -0500

    Creacion del proyecto optimaStock/se sube al repositorio en github

---
+++ src/main/java/com/hrks/OptimaStock/OptimaStockApplication.java
@@ -0,0 +1,13 @@
+package com.hrks.OptimaStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimaStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimaStockApplication.class, args);
+	}
+
+}
