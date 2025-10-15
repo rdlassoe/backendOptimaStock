76a0680a1cd6ad4e438c70abed4dbe3640713567
AndresU1109 <elkinuribe2020@itp.edu.co>
2025-10-14 22:33:29 -0500

    mensaje de pruba funcionamiento en el main

--- src/main/java/com/hrks/OptimaStock/OptimaStockApplication.java
+++ src/main/java/com/hrks/OptimaStock/OptimaStockApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OptimaStockApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(OptimaStockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OptimaStockApplication.class, args);
+
+        System.out.println("¡Hola! Este es un mensaje de prueba en Java con IntelliJ IDEA.");
+
+    }
+
 }
